e356e01deff766b8b130197777cec119d5fe431a
trs1323 <45774674+trs1323@users.noreply.github.com>
2019-09-08 15:52:37 -0700

    Updates

---
+++ precache-manifest.021214ff22a41942ea3be68d6aa19d7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b39b15624061a32808d2098ec07fd2b",
+    "url": "/pokemon_gen_react/index.html"
+  },
+  {
+    "revision": "7ba962d1dcc70786d54c",
+    "url": "/pokemon_gen_react/static/css/main.7b1b2d64.chunk.css"
+  },
+  {
+    "revision": "9ff2bd857a39c61afda9",
+    "url": "/pokemon_gen_react/static/js/2.e5031cf8.chunk.js"
+  },
+  {
+    "revision": "7ba962d1dcc70786d54c",
+    "url": "/pokemon_gen_react/static/js/main.bf385cf7.chunk.js"
+  },
+  {
+    "revision": "416318a906d44e51826a",
+    "url": "/pokemon_gen_react/static/js/runtime~main.de01a2e7.js"
+  }
+]);
